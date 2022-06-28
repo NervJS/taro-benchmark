e9a6905e5880bbfdc51c62b4777095b3f5b7b887
chenjiajian <798095202@qq.com>
2022-06-28 21:47:37 +0800

    add Taro v3.5

--- taro/src/app.jsx
+++ taro/src/app.jsx
@@ -1,27 +1,7 @@
-import Taro, { Component } from '@tarojs/taro'
-import Index from './pages/index'
-
+import { Component } from 'react'
 import './app.scss'
 
-// 如果需要在 h5 环境中开启 React Devtools
-// 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
-
 class App extends Component {
-
-  config = {
-    pages: [
-      'pages/index/index'
-    ],
-    window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
-    }
-  }
 
   componentDidMount () {}
 
@@ -31,13 +11,10 @@ class App extends Component {
 
   componentDidCatchError () {}
 
-  // 在 App 类中的 render() 函数没有实际作用
-  // 请勿修改此函数
+  // this.props.children 是将要会渲染的页面
   render () {
-    return (
-      <Index />
-    )
+    return this.props.children
   }
 }
 
-Taro.render(<App />, document.getElementById('app'))
+export default App
--- taro/src/pages/index/index.jsx
+++ taro/src/pages/index/index.jsx
@@ -1,137 +1,151 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Image } from '@tarojs/components'
-import fetchData from '../../models'
+import { Component } from 'react'
+import { View, Button } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 
 import './index.scss'
 
-const startTime = Date.now()
+Taro.options.debug = true
 
-export default class Index extends Component {
-  state = {
-    data: [],
-    selected: 0,
-  }
+const random = (max) => Math.round(Math.random() * 1000) % max
 
-  config = {
-    navigationBarTitleText: 'Taro'
-  }
+const A = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean",
+  "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive",
+  "cheap", "expensive", "fancy"]
+const C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"]
+const N = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse",
+  "keyboard"]
 
-  componentWillMount () {
-    this.setState({
-      data: fetchData(),
-      selected: 0
-    }, () => {
-      Taro.showToast({
-        icon: 'none',
-        title: `init => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
-  }
+let nextId = 1
 
-  run = () => {
-    const startTime = Date.now()
-    this.setState({
-      data: fetchData(),
-      selected: 0
-    }, () => {
-      Taro.showToast({
-        icon: 'none',
-        title: `run => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
-  }
+const buildData = (count) => {
+  const data = new Array(count)
 
-  add = () => {
-    const startTime = Date.now()
-    this.setState({
-      data: this.state.data.concat(fetchData().slice(0, 20))
-    }, () => {
-      Taro.showToast({
-        icon: 'none',
-        title: `add => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
-  }
-
-  update = () => {
-    const startTime = Date.now()
-    const data = this.state.data
-    for (let i = 0; i < data.length; i += 10) {
-      const item = data[i]
-      data[i] = {
-        ...item,
-        title: `${item.title}!!!`
-      }
+  for (let i = 0; i < count; i++) {
+    data[i] = {
+      id: nextId++,
+      label: `${A[random(A.length)]} ${C[random(C.length)]} ${N[random(N.length)]}`,
     }
-    this.forceUpdate(() => {
-      Taro.showToast({
-        icon: 'none',
-        title: `update => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
   }
 
-  swapRows = () => {
-    const startTime = Date.now()
-    const data = this.state.data
-    if (data.length !== 400) return
-    let temp = data[1]
-    data[1] = data[398]
-    data[398] = temp
-    this.forceUpdate(() => {
-      Taro.showToast({
-        icon: 'none',
-        title: `swapRows => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
+  return data
+}
+
+const initialState = { data: [], selected: 0 }
+
+class Row extends Component {
+  shouldComponentUpdate(nextProps) {
+    const { item, selected } = this.props
+
+    return nextProps.item !== item || nextProps.selected !== selected
   }
 
-  select = (idx) => {
-    const startTime = Date.now()
-    this.setState({
-      selected: idx
-    }, () => {
-      Taro.showToast({
-        icon: 'none',
-        title: `select => 回调时间：${Date.now() - startTime}ms`,
-        duration: 1500,
-      })
-    })
+  onSelect = () => {
+    const { item, dispatch } = this.props
+
+    dispatch({ type: 'SELECT', id: item.id })
   }
 
-  clear = () => {
-    this.setState({ data: [], selected: 0 })
+  onRemove = () => {
+    const { item, dispatch } = this.props
+
+    dispatch({ type: 'REMOVE', id: item.id })
   }
 
   render() {
+    const { item } = this.props
+
     return (
-      <View className='container'>
-        <View className='jumbotron'>
-          <Button onClick={this.run}>Create 40 goods</Button>
-          <Button onClick={this.add}>Append 20 goods</Button>
-          <Button onClick={this.update}>Update every 10th good</Button>
-          <Button onClick={this.swapRows}>Swap good</Button>
-          <Button onClick={this.clear}>Clear</Button>
+      <View className="row">
+        <View className="row-1">{item.id}</View>
+        <View className="row-2">
+          <View>{item.label}</View>
         </View>
-        <View className='list'>
-          {
-            this.state.data.map((item, idx) => {
-              return (
-                <View className={idx % 2 ? (this.state.selected === idx ? 'item selected right' : 'item right') : (this.state.selected === idx ? 'item selected' : 'item')} key={idx} >
-                  <View className='image_wrap' onClick={this.select.bind(this, idx)}>
-                    <Image className='image' src={item.img} mode='aspectFit' />
-                  </View>
-                  <View class='title'>{item.title}</View>
-                  <View class='price'>¥ {item.price}</View>
-                </View>
-              )
-            })
-          }
+        <View className="row-3" onClick={this.onRemove}>
+          delete
+        </View>
+      </View>
+    )
+  }
+}
+
+class Jumbotron extends Component {
+  shouldComponentUpdate () {
+    return false
+  }
+
+  render() {
+    const { dispatch } = this.props
+
+    return (
+      <View className="jumbotron">
+        <View className="jumbotron-title">React keyed</View>
+        <View className="jumbotron-row">
+          <Button className='jumbotron-row-button' type="primary" id="run" onClick={() => dispatch({ type: 'RUN' })} >Create 500 rows</Button>
+          <Button className='jumbotron-row-button' type="primary" id="runlots" onClick={() => dispatch({ type: 'RUN_LOTS' })} >Create 1,000 rows</Button>
+          <Button className='jumbotron-row-button' type="primary" id="add" onClick={() => dispatch({ type: 'ADD' })} >Append 500 rows</Button>
+          <Button className='jumbotron-row-button' type="primary" id="update" onClick={() => dispatch({ type: 'UPDATE' })} >Update every 10th row</Button>
+          <Button className='jumbotron-row-button' type="primary" id="clear" onClick={() => dispatch({ type: 'CLEAR' })} >Clear</Button>
+          <Button className='jumbotron-row-button' type="primary" id="swaprows" onClick={() => dispatch({ type: 'SWAP_ROWS' })} >Swap Rows</Button>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default class Main extends Component {
+  state = initialState
+
+  dispatch = (action) => {
+    const { data } = this.state
+    console.log('click time: ', Date.now())
+
+    switch (action.type) {
+      case 'RUN':
+        return this.setState({ data: buildData(500), selected: 0 })
+      case 'RUN_LOTS':
+        return this.setState({ data: buildData(1000), selected: 0 })
+      case 'ADD':
+        return this.setState({ data: data.concat(buildData(500))})
+      case 'UPDATE': {
+        const newData = data.slice(0)
+
+        for (let i = 0; i < newData.length; i += 10) {
+          const r = newData[i]
+
+          newData[i] = { id: r.id, label: r.label + " !!!" }
+        }
+
+        return this.setState({ data: newData })
+      }
+      case 'CLEAR':
+        return this.setState({ data: [], selected: 0 })
+      case 'SWAP_ROWS': {
+        if (data.length > 498) {
+          return this.setState({ data: [data[0], data[498], ...data.slice(2, 498), data[1], data[499]] })
+        }
+
+        return
+      }
+      case 'REMOVE': {
+        const idx = data.findIndex((d) => d.id === action.id)
+
+        return this.setState({ data: [...data.slice(0, idx), ...data.slice(idx + 1)] })
+      }
+      case 'SELECT':
+        return this.setState({ selected: action.id })
+    }
+  }
+
+  render() {
+    const { data, selected } = this.state
+
+    return (
+      <View className="container">
+        <Jumbotron dispatch={this.dispatch} />
+        <View>
+          {data.map((item) => (
+            <Row key={item.id} item={item} selected={selected === item.id} dispatch={this.dispatch} />
+          ))}
         </View>
       </View>
     )
